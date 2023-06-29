3360e645a88e6975757d25e65cd7d4ebd174323c
Stella Laurenzo <stellaraccident@gmail.com>
2023-06-28 18:04:45 -0700

    Implement basic compiler API.

--- iree/engine/support/compiler_api.py
+++ iree/engine/support/compiler_api.py
@@ -4,10 +4,11 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+from contextlib import closing
+from pathlib import Path
+from typing import Sequence, Optional, Union
 
 from . import compiler_dl as dl
-
-from threading import Lock
 
 __all__ = ["Compiler"]
 
@@ -21,9 +22,62 @@ class Compiler:
     """
 
     def __init__(self):
-        self._lock = Lock()
         self._session = dl.Session()
 
-    def open_source_buffer(self, buffer):
+    def set_flags(self, *flags: Sequence[str]):
+        self._session.set_flags(*flags)
+
+    def load_buffer(self, buffer, *, buffer_name: Optional[str]) -> "Pipeline":
         """Opens a source backed by a buffer in memory."""
-        ...
+        source = dl.Source.wrap_buffer(self._session, buffer, buffer_name=buffer_name)
+        return self._start_pipeline(source)
+
+    def load_file(self, file_path: Union[str, Path]) -> "Pipeline":
+        """Opens a source backed by a file."""
+        source = dl.Source.open_file(self._session, str(file_path))
+        return self._start_pipeline(source)
+
+    def _start_pipeline(self, source: dl.Source) -> "Pipeline":
+        inv = dl.Invocation(self._session)
+        inv.enable_console_diagnostics()  # TODO: Real diagnostics
+        # TODO: Crash handler, etc.
+        if not inv.parse_source(source):
+            # TODO: Capture diagnostics into exception.
+            raise ValueError("Error parsing source (see diagnostics)")
+        return Pipeline(self, inv)
+
+    def open_output_file(self, file_path: Union[str, Path]) -> dl.Output:
+        return dl.Output.open_file(str(file_path))
+
+    def open_output_membuffer(self) -> dl.Output:
+        return dl.Output.open_membuffer()
+
+
+class Pipeline:
+    """Encapsulates a compilation pipeline in progress."""
+
+    def __init__(self, compiler: Compiler, inv: dl.Invocation):
+        self._compiler = compiler
+        self._inv = inv
+
+    def __del__(self):
+        self.close()
+
+    def close(self):
+        if self._inv:
+            self._inv.close()
+            self._inv = None
+
+    def execute(
+        self,
+        pipeline_type: dl.PipelineType = dl.PipelineType.IREE_COMPILER_PIPELINE_STD,
+    ):
+        if not self._inv.execute(pipeline_type):
+            # TODO: Capture diagnostics into exception.
+            raise ValueError("Error parsing source (see diagnostics)")
+
+    def output_ir(self, output: dl.Output):
+        self._inv.output_ir(output)
+
+    def output_vm_bytecode(self, output: dl.Output):
+        self._inv.output_vm_bytecode(output)
--- tests/support/compiler_test.py
+++ tests/support/compiler_test.py
@@ -4,6 +4,9 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+from contextlib import closing
+from pathlib import Path
+import tempfile
 import unittest
 
 from iree.engine.support.compiler_dl import *
@@ -46,11 +49,92 @@ class DlInvocationTest(unittest.TestCase):
     session = Session()
     inv = session.invocation()
 
+class DlOutputTest(unittest.TestCase):
+
+  def testOpenMembuffer(self):
+    out = Output.open_membuffer()
+
+  def testOpenMembufferExplicitClose(self):
+    out = Output.open_membuffer()
+    out.close()
+
+  def testOpenMembufferWrite(self):
+    out = Output.open_membuffer()
+    out.write(b"foobar")
+    mem = out.map_memory()
+    self.assertEqual(b"foobar", bytes(mem))
+    out.close()
+
+  def testOpenFileNoKeep(self):
+    file_path = tempfile.mktemp()
+    out = Output.open_file(file_path)
+    try:
+      out.write(b"foobar")
+      self.assertTrue(Path(file_path).exists())
+    finally:
+      out.close()
+      # Didn't call keep, so should be deleted.
+    self.assertFalse(Path(file_path).exists())
+
+  def testOpenFileKeep(self):
+    file_path = tempfile.mktemp()
+    out = Output.open_file(file_path)
+    try:
+      try:
+        out.write(b"foobar")
+        out.keep()
+      finally:
+        out.close()
+        # Didn't call keep, so should be deleted.
+
+      with open(file_path, "rb") as f:
+        contents = f.read()
+        self.assertEqual(b"foobar", contents)
+    finally:
+      Path(file_path).unlink()
+
 class CompilerAPITest(unittest.TestCase):
 
   def testCreate(self):
     compiler = Compiler()
 
+  def testLoadFromBytes(self):
+    compiler = Compiler()
+    p = compiler.load_buffer("module {}".encode(), buffer_name="foobar")
+
+  def testPipelineClose(self):
+    compiler = Compiler()
+    p = compiler.load_buffer("module {}".encode(), buffer_name="foobar")
+    p.close()
+
+  def testLoadFromFile(self):
+    compiler = Compiler()
+    with tempfile.NamedTemporaryFile("w", delete=False) as tf:
+      tf.write("module {}")
+      tf.close()
+      p = compiler.load_file(tf.name)
+      p.close()
+
+  def testExecuteIR(self):
+    compiler = Compiler()
+    p = compiler.load_buffer("module {}".encode(), buffer_name="foobar")
+    p.execute()
+    with closing(compiler.open_output_membuffer()) as output:
+      p.output_ir(output)
+      ir_contents = bytes(output.map_memory())
+      print(ir_contents)
+      self.assertEqual(b'module {\n}', ir_contents)
+
+  def testExecuteVMFB(self):
+    compiler = Compiler()
+    compiler.set_flags("--iree-hal-target-backends=vmvx")
+    p = compiler.load_buffer("module {func.func @main(%arg0: i32) -> (i32) {return %arg0 : i32}}".encode(), buffer_name="foobar")
+    p.execute()
+    with closing(compiler.open_output_membuffer()) as output:
+      p.output_vm_bytecode(output)
+      ir_contents = bytes(output.map_memory())
+      print(len(ir_contents))
+      self.assertGreater(len(ir_contents), 0)
 
 if __name__ == "__main__":
   unittest.main()
